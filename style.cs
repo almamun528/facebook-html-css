dfeb907dc6acd77ebbedcf8e37c8249607aa5a41
almamun528 <ayapths@gmail.com>
2024-09-12 14:07:29 +0600

    basic setup

---
+++ style.cs
@@ -0,0 +1,12 @@
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+}
+:root{
+    --text-color:rgba(255, 255, 255, 0.5);
+    --title-color:#ffffff;
+    --primary-color:rgba(230, 83, 60, 1);
+    
+}
